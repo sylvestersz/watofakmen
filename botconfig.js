68e07ea2b7924d95810c144d3a17234c15a8e6c8
Sylvestersz <39089464+sylvestersz@users.noreply.github.com>
2021-12-03 06:50:57 +0700

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -1,12 +1,12 @@
 module.exports = {
   Admins: ["405543536042311701"], //Admins of the bot
   ExpressServer: true, //If you wanted to make the website run or not
-  DefaultPrefix: process.env.Prefix || "-", //Default prefix, Server Admins can change the prefix
+  DefaultPrefix: process.env.Prefix || "", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token: process.env.Token || "OTE2MDUwNDg5ODQ4NzY2NDg0.YakggA.4K85VhEqRMnQF_KRKowT-5Pcxv8", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "916050489848766484", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "jLNKNVDq_y6IiZO2ctYhUbvrlN6_mbVK", //Discord Client Secret
+  Token: process.env.Token || "", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   ServerDeafen: true, //If you want bot to stay deafened
   DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
@@ -28,15 +28,15 @@ module.exports = {
   //Lavalink
   Lavalink: {
     id: "Main",
-    host: "host name or IP",
-    port: 80, // The port that lavalink is listening to. This must be a number!
-    pass: "your password",
-    secure: false, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
+    host: "lavalink2.devz.cloud",
+    port: 443, // The port that lavalink is listening to. This must be a number!
+    pass: "mathiscool",
+    secure: true, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
   },
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "3f27651f6b1d4261be1d224d72a9bed9", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "48aec246d4f5480a8f31996505f3aea2", //Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
   },
 };
